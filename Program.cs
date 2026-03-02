039bd190c5073a7bb89bf29a83287dbfea396985
Yash Chavan <yashchavan1818@gmail.com>
2026-03-02 14:22:14 +0530

    Connected backend to Supabase PostgreSQL

--- Program.cs
+++ Program.cs
@@ -108,7 +108,8 @@ namespace EventManagement.API
 
             // DbContext
             builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // JWT Authentication
             builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
